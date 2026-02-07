00af3cee62ab2240c9d68c0f7e26455612cc56e6
BigElderK <BigElderK@divineland.org>
2026-02-07 17:29:00 +0800

    init

---
+++ public/include/interface/script/script.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <functional>
+#include <memory>
+#include <variant>
+
+namespace Arieo::Interface::Script
+{
+    // Forward declarations
+    class IContext;
+    class IModule;
+    class IInstance;
+    class IInterface;
+    
+    class IContext
+    {
+    public:
+        virtual void addHostFunction(
+            const std::string& module_name,
+            const std::string& function_name,
+            const std::function<void()>& function
+        ) = 0;
+    };
+
+    class IModule
+    {
+    public:
+    };
+
+    class IInstance
+    {
+    public:
+        virtual void* queryInterface(const std::string& interface_name) = 0;
+        virtual void* queryFunction(void* interface, const std::string& function_name) = 0;
+        virtual void callFunction(
+            void*) = 0;
+    };
+
+    class IScriptEngine
+    {
+    public:
+        virtual IContext* createContext() = 0;
+        virtual void destroyContext(IContext* context) = 0;
+
+        virtual void initInterfaceLinkers(const std::filesystem::path& lib_file_path) = 0;
+        
+        virtual IModule* loadModuleFromWatString(const std::string& wat_string) = 0;
+        virtual IModule* loadModuleFromCompiledBinary(void* binary_data, size_t data_size) = 0;
+        virtual void unloadModule(IModule* module) = 0;
+
+        virtual IInstance* createInstance(IContext* context, IModule* module) = 0;
+        virtual void destroyInstance(IInstance* instance) = 0;
+    };
+}
